77140b73da807c40ae46fae06cfcd83d220cbeed
cristhiandas <cristhiandf94@gmail.com>
2017-11-24 16:43:18 +0000

    Saving stuff suffering pain done (now it saves the status in sessions, that way if you don't reset the host it will have the last values)

--- app.rb
+++ app.rb
@@ -8,5 +8,20 @@ class Thermostat < Sinatra::Base
     erb(:index)
   end
 
+  get '/temperature' do
+    content_type :json
+    { temperature: session[:temperature] , city: session[:city]}.to_json
+  end
+
+  post '/temperature' do
+    session[:temperature] = params[:temperature]
+    200
+  end
+
+  post '/city' do
+    session[:city] = params[:city]
+    200
+  end
+
   run! if app_file == $0
 end
--- public/src/jquery.js
+++ public/src/jquery.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
 
+  function postingTemp (temp) {
+  $.post('/temperature', {temperature: temp}, function(data) {
+  });
+}
+
   function update(){
     $('#temp').html(thermostat.temperature());
     $('#usage').html(thermostat.usage());
     $('#usage').attr('class', thermostat.usage());
-
+    postingTemp(thermostat.temperature());
     $('#mode').html(function(){
       if(thermostat._isOn === true){
         return 'On';
@@ -13,7 +18,18 @@ $(document).ready(function(){
     });
   }
 
+  function postCity(city) {
+    $.post('/city', {city: city}, function(data) {
+    })
+  }
+
   update();
+
+    $('#sendcity').click(function() {
+      var city = $('#city').val();
+      getLocalTemp(city);
+      postCity(city);
+    });
 
    $('#up').click(function(){
      thermostat.up();
@@ -37,18 +53,40 @@ $(document).ready(function(){
 
 
 
-  $('#select').submit(function(event){
-    event.preventDefault();
-    var currentCity = $('#city').val();
-    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=metric&APPID=029834736f10a5ed0db464e2f28cddb9", function(data){
+  function getTempFromApi() {
+  $.get('/temperature', function(data) {
+    if (data.temperature === null) {
+      $('#temp').html(thermostat.temperature());
+    } else {
+      thermostat._temperature = data.temperature
+      $('#temp').html(thermostat.temperature());
+    }
+  });
+}
 
+function getCityFromApi() {
+  $.get('/temperature', function(data) {
+    if (data.city === null) {
+      $('#currentcity').html('London');
+      getLocalTemp();
+    } else {
+      $('#currentcity').html(data.city);
+      getLocalTemp(data.city);
+    }
+  });
+}
+
+
+function getLocalTemp(city = 'London'){
+    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=029834736f10a5ed0db464e2f28cddb9", function(data){
       $('#outsideTemp').html(data.main.temp);
-      $('#currentcity').html(currentCity);
+      $('#currentcity').html(city);
     }).fail(function() {
       $('#currentcity').html('City not found');
       $('#outsideTemp').html('N/A');
     });
-  })
+}
 
-
+getCityFromApi();
+getTempFromApi();
 });
